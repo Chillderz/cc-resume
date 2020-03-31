d4e8d0048f06efee74816d9546b1a8b7a4d68ade
Christopher Childers <cchilders1289@gmail.com>
2020-03-30 22:12:18 -0400

    changed the order of Work Experience to better match my resume

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -1,18 +1,18 @@
 const WORK_EXPERIENCE = [
     {
         'id': 1,
-        'company': 'Epic',
+        'company': 'AWS',
         'title': {
-            'name': 'Client Systems - Technical Services',
-            'years': '2012-2017'
+            'name': 'Cloud Infrastructure Architect',
+            'years': '2019-present'
         }
     },
     {
         'id': 2,
         'company': 'WorkWave',
         'title': {
-            'name': 'Sr. Systems Engineer',
-            'years': '2017-2018'
+            'name': 'Sr. DevOps Engineer',
+            'years': '2019'
         }
     },
     {
@@ -35,18 +35,18 @@ const WORK_EXPERIENCE = [
         'id': 5,
         'company': 'WorkWave',
         'title': {
-            'name': 'Sr. DevOps Engineer',
-            'years': '2019'
+            'name': 'Sr. Systems Engineer',
+            'years': '2017-2018'
         }
     },
     {
         'id': 6,
-        'company': 'AWS',
+        'company': 'Epic',
         'title': {
-            'name': 'Cloud Infrastructure Architect',
-            'years': '2019-present'
+            'name': 'Client Systems - Technical Services',
+            'years': '2012-2017'
         }
-    }
+    },
 ]
 
 export default WORK_EXPERIENCE;
