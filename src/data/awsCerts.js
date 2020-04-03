7090f51f7c8a5fcee4e29a50a6db97dae96ecaba
Christopher Childers <cchilders1289@gmail.com>
2020-04-03 19:09:35 -0400

    Added better links for certificate validation and fixed a Developer typo

--- src/data/awsCerts.js
+++ src/data/awsCerts.js
@@ -2,49 +2,49 @@ const AWS_CERTIFICATIONS = [
     {
         id: 1,
         title: 'Solutions Architect - Professional',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=4&t=c&d=2019-09-20&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: '37VWC6XKE24EQ0KJ'
     },
     {
         id: 2,
         title: 'DevOps Engineer - Professional',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=5&t=c&d=2018-10-30&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: '7KNQTNNKKBFE1C5R'
     },
     {
         id: 3,
         title: 'Security - Specialty',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=7&t=c&d=2020-01-03&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: '0FTDC7HCNMFQ1V95'
     },
     {
         id: 4,
         title: 'Advanced Networking - Specialty',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=6&t=c&d=2020-02-27&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: 'VPHZ03H1NF1Q1S5B'
     },
     {
         id: 5,
         title: 'Solutions Architect - Associate',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=1&t=c&d=2018-02-19&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: 'KZF7HR41LF1113WK'
     },
     {
         id: 6,
         title: 'SysOps Administrator - Associate',
-        link: 'http://aws.amazon.com/verification',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=3&t=c&d=2018-06-25&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: 'X2TSNFVCLJRQQRGY'
     },
     {
         id: 7,
-        title: 'Devloper - Associate',
-        link: 'http://aws.amazon.com/verification',
+        title: 'Developer - Associate',
+        link: 'https://www.certmetrics.com/amazon/public/badge.aspx?i=2&t=c&d=2018-05-21&ci=AWS00434749',
         image: '../images/aws-certified.png',
         validation: 'HGW4ZZQKB2QEQN5Y'
     }
